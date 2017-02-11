7c5582e9c09184e160b0e2f28cdb46d6d67b92a2
Yinrong "David" Liang <liang162@purdue.edu>
2017-02-11 17:38:16 -0500

    search function

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UIScrollViewDelegate {
     
     var businesses: [Business]!
+    var filteredBusinesses: [Business]!
     
     @IBOutlet weak var tableView: UITableView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,20 +22,17 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         tableView.dataSource=self
         tableView.rowHeight=UITableViewAutomaticDimension
         tableView.estimatedRowHeight=120 //scroll height
-        
         tableView.keyboardDismissMode=UIScrollViewKeyboardDismissMode.onDrag
+        
+        navigationController?.navigationBar.barTintColor = UIColor(red:0.12, green:0.12, blue:0.12, alpha:0.5)
         
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
-            self.tableView.reloadData()
-            if let businesses = businesses {
-                for business in businesses {
-                    print(business.name!)
-                    print(business.address!)
-                }
-            }
             
+            //search function prepare
+            self.filteredBusinesses = businesses
+            self.tableView.reloadData()
             }
         )
         
@@ -43,6 +40,8 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         searchBar.sizeToFit()
         searchBar.placeholder="Restaurants"
         navigationItem.titleView=searchBar
+        searchBar.delegate = self
+        searchBar.keyboardAppearance = .dark
         
         /* Example of Yelp search with more search options specified
          Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
@@ -57,15 +56,9 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         
     }
     
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if businesses != nil {
-            return businesses.count
+        if filteredBusinesses != nil {
+            return filteredBusinesses.count
         } else {
             return 0
         }
@@ -73,11 +66,50 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
-        
-        cell.business=businesses[indexPath.row]
+        cell.business=filteredBusinesses[indexPath.row]
         
         return cell
     }
+    
+    /******* search bar ********/
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredBusinesses = searchText.isEmpty ? businesses : businesses.filter({ (dataBusiness: Business) -> Bool in
+            let businessName = dataBusiness.name! as String
+            return businessName.range(of: searchText, options: .caseInsensitive) != nil
+        })
+        self.tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    /******* search bar ********/
+    
+    /****** infinite scroll ********/
+    var alreadyMadeRequestToAPI = false
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if(!alreadyMadeRequestToAPI) {
+            let screenHeight = tableView.contentSize.height
+            let scrollLimit = screenHeight - tableView.bounds.size.height
+            
+            if(scrollView.contentOffset.y > scrollLimit && tableView.isDragging) {
+                alreadyMadeRequestToAPI = true
+                loadMoreData()
+            }
+        }
+    }
+    
+    func loadMoreData() {
+        
+    }
+    /****** infinite scroll ********/
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     /*
      // MARK: - Navigation
      
